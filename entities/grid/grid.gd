2b07d51df45a34b011dabc5abfdfe37e1bb0c27d
H. Hernan Saez <hhsaez@gmail.com>
2024-04-13 09:14:27 -0300

    Select grid cells

--- entities/grid/grid.gd
+++ entities/grid/grid.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+signal cells_selected(cells)
+
+var selected_cells = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,8 +11,13 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		if selected_cells.size() >= 2:
+			cells_selected.emit(selected_cells)
+		selected_cells = []
 
-
-func _on_cell_hovered(which):
-	print("Cell hovered %s", which.name)
+func _on_cell_hovered(cell):
+	if not selected_cells.is_empty():
+		if selected_cells[selected_cells.size() - 1] == cell.name:
+			return
+	selected_cells.append(cell.name)
